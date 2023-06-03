4c1ee9c43369611a090f415342ef04d8493fd8fa
Sheggybtw <pschenichniyvitaliy@gmail.com>
2023-06-03 13:54:33 +0300

    main concept , with adding new item to list func

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+let items = ["Do smth", "Do smth", "Do smth"];
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res) {
+
+    const today = new Date();
+
+    let options = {
+        weekday : "long",
+        day : "numeric",
+        month : "long"
+    };
+
+    let day = today.toLocaleDateString("en-US", options) 
+
+t
+    res.render('list', { kindOfDay: day , newListItems: items});
+});
+
+
+app.post("/", function(req, res) {
+   let item = req.body.newItem;
+
+       items.push(item)
+
+    res.redirect("/");
+})
+
+app.listen(3000, function() {
+
+})
+
